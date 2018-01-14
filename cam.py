14944da661f6ea42e8a788702e710b10098a8e70
snooty7 <trankov86@gmail.com>
2018-01-14 23:02:41 +0200

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,15 +1,15 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(1)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
 out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
 while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     out.write(frame)
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
